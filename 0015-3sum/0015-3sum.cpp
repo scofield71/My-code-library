6f50ba915f1c6c37081d1978c941cef00eaec7ec
scofield71 <107502142+scofield71@users.noreply.github.com>
2026-05-07 23:25:25 +0530

    Time: 59 ms (19.99%), Space: 39.9 MB (9.6%) - LeetHub

---
+++ 0015-3sum/0015-3sum.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<vector<int>> twoSum(vector<int>& nums,int i,int j,int target) {
+        vector<vector<int>> ans;
+        while(i<j) {
+            int t = nums[i]+nums[j];
+            if(t>target) j--;
+            else if(t<target) i++;
+            else {
+                ans.push_back({nums[i],nums[j]});
+                i++;
+                j--;
+                while(i<nums.size() && nums[i]==nums[i-1]) i++;
+                while(j>0 && nums[j]==nums[j+1]) j--;
+            }
+        }
+        return ans;
+    }
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> ans;
+        for(int i=0;i<nums.size();i++) {
+            vector<vector<int>> tmp = twoSum(nums,i+1,nums.size()-1,-1*nums[i]);
+            for(auto it:tmp) {
+                it.push_back(nums[i]);
+                ans.push_back(it);
+            }
+            i++;
+            while(i<nums.size() && nums[i]==nums[i-1] ) i++;
+            i--;
+        }
+        return ans;
+    }
+};
