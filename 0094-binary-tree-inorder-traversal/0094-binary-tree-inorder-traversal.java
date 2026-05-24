c5de2b26b2bb279ab7b0ff8907daae3117d44a69
scofield71 <107502142+scofield71@users.noreply.github.com>
2026-05-24 21:57:51 +0530

    Time: 0 ms (100%), Space: 43.4 MB (14.35%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -22,7 +22,26 @@ class Solution {
     }
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> ans = new ArrayList<>();
-        helper(root,ans);
+        TreeNode traverse = root;
+        while(traverse!=null) {
+            TreeNode nearestLeft = traverse.left;
+            while(nearestLeft!=null && nearestLeft.right!=traverse) {
+                if(nearestLeft.left==null && nearestLeft.right==null) break;
+                if(nearestLeft.right!=null) nearestLeft=nearestLeft.right;
+                else break;
+            }
+            if(nearestLeft==null) {
+                ans.add(traverse.val);
+                traverse=traverse.right;
+            } else if(nearestLeft.right==traverse) {
+                nearestLeft.right=null;
+                ans.add(traverse.val);
+                traverse=traverse.right;
+            } else if(nearestLeft.right==null) {
+                nearestLeft.right=traverse;
+                traverse=traverse.left;
+            }
+        }
         return ans;
     }
 }
