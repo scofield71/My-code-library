638be9c18f7d52edbb4e30c090393d3f6755511c
scofield71 <107502142+scofield71@users.noreply.github.com>
2026-05-29 23:25:32 +0530

    Time: 7 ms (27.1%), Space: 22 MB (11.04%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -3,11 +3,15 @@ public:
     int jump(vector<int>& nums) {
         vector<int> dp(nums.size(),INT_MAX);
         dp[0]=0;
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+        pq.push({0,nums[0]});
         for(int i=1;i<nums.size();i++) {
-            for(int j=i-1;j>=0;j--) {
-                int maxReach = nums[j]+j;
-                if(maxReach>=i) dp[i]=min(dp[i],dp[j]+1);
+            while(pq.size()>0 && pq.top().second<i) pq.pop();
+            if(pq.size()>0) {
+                dp[i]=pq.top().first+1;
+                pq.push({dp[i],i+nums[i]});
             }
+            else dp[i]=INT_MAX;
         }
         return dp[nums.size()-1];
     }
