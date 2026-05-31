110de6a0f9aa5fe0f92825ad29c36adbf94ea619
scofield71 <107502142+scofield71@users.noreply.github.com>
2026-05-31 11:57:43 +0530

    Time: 681 ms (5.13%), Space: 56 MB (5.92%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPossible(vector<int>& nums, int curNdx,vector<int>& mem) {
+        if(curNdx==nums.size()-1) return true;
+        if(mem[curNdx]!=-1) return mem[curNdx]==0?false:true;
+        for(int i=curNdx+1;i<=(curNdx+nums[curNdx]) && i<nums.size();i++) {
+            bool possible = isPossible(nums,i,mem);
+            if(possible) return mem[curNdx]=true;
+        }
+        return mem[curNdx]=false;
+    }
+    bool canJump(vector<int>& nums) {
+        vector<int> mem(nums.size()+1,-1);
+        return isPossible(nums,0,mem);
+    }
+};
