c0e3f020d7c4b8de45d7400e267f97a13adbc6e7
scofield71 <107502142+scofield71@users.noreply.github.com>
2026-05-31 07:42:45 +0530

    Time: 19 ms (7.1%), Space: 47.4 MB (5.18%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-    bool isCyclic(vector<vector<int>>& outDegree, vector<int>& cycleVisited, vector<int>& visited, int node) {
+    bool isCyclePresent(unordered_map<int,vector<int>>& ourmap, vector<int>& visited, vector<int>& cycleVisited, int node) {
         if(cycleVisited[node]==1) return true;
         if(visited[node]==1) return false;
         visited[node]=1;
         cycleVisited[node]=1;
-        for(int i=0;i<outDegree[node].size();i++) {
-            if(cycleVisited[outDegree[node][i]]==1) return true;
-            if(visited[outDegree[node][i]]==1) continue;
-            bool tmpCycle = isCyclic(outDegree,cycleVisited,visited,outDegree[node][i]);
-            if(tmpCycle) return true;
+        for(auto it:ourmap[node]) {
+            bool isCycle = isCyclePresent(ourmap,visited,cycleVisited,it);
+            if(isCycle) return true;
         }
         cycleVisited[node]=0;
         return false;
     }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> outDegree(numCourses+1,vector<int>());
-        for(auto it:prerequisites) outDegree[it[1]].push_back(it[0]);
-        vector<int> visited(numCourses+1,0);
-        for(int i=1;i<=numCourses;i++) {
+        unordered_map<int,vector<int>> ourmap;
+        for(auto it:prerequisites) {
+            ourmap[it[1]].push_back(it[0]);
+        }
+        vector<int> visited(numCourses,0);
+        for(int i=0;i<numCourses;i++) {
             if(visited[i]==0) {
-                vector<int> cycleVisited(numCourses+1,0);
-                bool cyclePresent = isCyclic(outDegree,cycleVisited, visited, i);
-                if(cyclePresent) return false;
+                vector<int> cycleVisited(numCourses,0);
+                bool isCycle = isCyclePresent(ourmap,visited,cycleVisited,i);
+                if(isCycle) return false;
             }
         }
         return true;
