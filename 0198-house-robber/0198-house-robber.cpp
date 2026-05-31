3b974a8766034d6d42d07d0a74270b44116ce026
scofield71 <107502142+scofield71@users.noreply.github.com>
2026-05-31 15:13:05 +0530

    Time: 0 ms (100%), Space: 10.8 MB (20.97%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        vector<int> dp(nums.size(),0);
+        dp[0]=nums[0];
+        for(int i=1;i<nums.size();i++) {
+            if(i==1) dp[i]=max(dp[i-1],nums[i]);
+            else dp[i]=max(dp[i-1],dp[i-2]+nums[i]);
+        }
+        return dp[nums.size()-1];
+    }
+};
