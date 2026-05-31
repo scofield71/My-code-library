60e0afb40ff3017049905fcd6c31a219044faf8b
scofield71 <107502142+scofield71@users.noreply.github.com>
2026-05-31 12:03:18 +0530

    Time: 0 ms (100%), Space: 52.3 MB (82.55%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -10,19 +10,11 @@ public:
         return mem[curNdx]=false;
     }
     bool canJump(vector<int>& nums) {
-        // vector<int> mem(nums.size()+1,-1);
-        // return isPossible(nums,0,mem);
-        vector<bool> dp(nums.size()+1,false);
-        dp[0]=true;
+        int range = nums[0];
         for(int i=1;i<nums.size();i++) {
-            
-            for(int j=i-1;j>=0;j--) {
-                if(dp[j] && j+nums[j]>=i) {
-                    dp[i]=true;
-                    break;
-                } 
-            }
+            if(range<i) return false;
+            range = max(range,i+nums[i]);
         }
-        return dp[nums.size()-1];
+        return true;
     }
 };
