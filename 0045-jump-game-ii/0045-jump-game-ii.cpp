e4a56c3fdc6a1f74ab0a789a2b9815bf77273857
scofield71 <107502142+scofield71@users.noreply.github.com>
2026-05-31 12:08:45 +0530

    Time: 0 ms (100%), Space: 20.4 MB (61.26%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        vector<int> dp(nums.size(),INT_MAX);
-        dp[0]=0;
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
-        pq.push({0,nums[0]});
+        if(nums.size()==1) return 0;
+        int range = nums[0];
+        int jumps = 1;
+        int maxRange = nums[0];
         for(int i=1;i<nums.size();i++) {
-            while(pq.size()>0 && pq.top().second<i) pq.pop();
-            if(pq.size()>0) {
-                dp[i]=pq.top().first+1;
-                pq.push({dp[i],i+nums[i]});
+            if(i>range) {
+                range = maxRange;
+                maxRange = max(maxRange,i+nums[i]);
+                jumps++;
+            } else {
+                maxRange = max(maxRange,i+nums[i]);
             }
-            else dp[i]=INT_MAX;
         }
-        return dp[nums.size()-1];
+        return jumps;
     }
 };
