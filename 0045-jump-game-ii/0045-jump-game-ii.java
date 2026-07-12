f22bab2884d2d785bd88534ac1f3d3891fddeefb
scofield71 <107502142+scofield71@users.noreply.github.com>
2026-07-12 20:23:08 +0530

    Time: 2 ms (31.4%), Space: 47 MB (87.85%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,17 +1,17 @@
 class Solution {
     public int jump(int[] nums) {
-        if(nums.length==1) return 0;
-        int[] dp = new int[nums.length];
-        for (int i=0;i<nums.length;i++) dp[i]=Integer.MAX_VALUE;
-        dp[0]=0;
-        dp[1]=(nums[0]==0)? dp[1] : 1;
-        for(int i=2;i<nums.length;i++) {
-            for (int j=i-1;j>=0;j--) {
-                if (dp[j]!=-1 && (nums[j]+j) >= i) {
-                    dp[i] = Math.min(dp[i],dp[j]+1);
-                }
+        int nextJump = 0;
+        int maxReachable = 0;
+        int jumps = 0;
+        for (int i=0;i<nums.length;i++) {
+            if (maxReachable<i) return -1;
+            maxReachable = Math.max(maxReachable,i+nums[i]);
+            if (i==nums.length-1) return jumps;
+            if (i==nextJump) {
+                jumps++;
+                nextJump = maxReachable;
             }
         }
-        return dp[nums.length-1]==Integer.MAX_VALUE ? -1 : dp[nums.length-1];
+        return jumps;
     }
 }
