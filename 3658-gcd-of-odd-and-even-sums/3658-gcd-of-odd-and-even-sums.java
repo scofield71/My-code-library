ba57a5f23ba6ea3cf543d6cc69f2498970fe19e1
scofield71 <107502142+scofield71@users.noreply.github.com>
2026-07-15 23:36:13 +0530

    Time: 0 ms (100%), Space: 42.8 MB (6.12%) - LeetHub

---
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int findGCD(int x,int y) {
+        return 1;
+        // if () return findGCD(y,x);
+
+    }
+    public int gcdOfOddEvenSums(int n) {
+        // int evenSum = n*(n+1);
+        // int oddSum = Math.pow(n,2);
+        return n;
+    }
+}
